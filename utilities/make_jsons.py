e9458d7855a6b2f0300ef297b40b9f974955586d
Mahesh Rao <raomahes@gmail.com>
2020-05-22 10:04:41 -0500

    update cohort.json and add fun images

--- utilities/make_jsons.py
+++ utilities/make_jsons.py
@@ -1,26 +1,48 @@
 from PIL import Image
 import os
+import pandas as pd
+import json
 
 
-def make_cohort_json(class_info):
+def prepare_df(df_path):
+    df = pd.read_csv(df_path)
+    if "First" not in df.columns:
+        df[["First", "Last"]] = df['Name'].str.replace('  ', ' ').str.split(' ', expand = True)
+    return df
+
+
+def clean_string(s):
+    return s.replace('“', '"').replace('”', '"').replace("’", "'").replace("—", "-")
+
+
+def make_cohort_dict(class_info):
     '''
     function to make cohort.json.
     arg class_info: pandas DataFrame, df including minimally the columns:
         First, Last, Tagline, Bio, Github, LinkedIn, and Capstone
     '''
-    cohort_json_2 = {"cohort": []}
+    cohort_json = {"cohort": []}
     for ind, row in class_info.iterrows():
         student_dict = {"id": ind,
                         "firstName": row['First'],
                         "lastName": row['Last'],
-                        "reelThemIn": "<p>"+str(row['Tagline']).replace('“', '"').replace('”', '"')+"</p>",
-                        "bio": "<p>"+str(row['Bio']).replace('“', '"').replace('”', '"')+"</p>",
+                        "reelThemIn": "<p>"+clean_string(str(row['Tagline']))+"</p>",
+                        "bio": "<p>"+clean_string(str(row['Bio']))+"</p>",
                         "github": row['Github'],
                         "linkedIn": row['LinkedIn'],
-                        "portfolio": str(row['Capstone']),
+                        "portfolio": str(row['Capstone (link)']),
                         "proImg": "../assets/img/{}1.jpg".format(row['First'].lower()),
                         "funImg": "../assets/img/{}2.jpg".format(row['First'].lower())}
-        cohort_json_2['cohort'].append(student_dict)
+        cohort_json['cohort'].append(student_dict)
+    return cohort_json
+
+
+def make_cohort_json(df_path):
+    df = prepare_df(df_path)
+    cohort_json = make_cohort_dict(df)
+    with open("cohort.json", "w") as outfile:
+        json.dump(cohort_json, outfile)
+    print("cohort.json created!")
 
 
 def convert_to_jpg(img_path):
